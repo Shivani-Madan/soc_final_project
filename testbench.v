5e78a5e4f2d1c8eed9356bed198dadc490b810d2
Shivani-Madan <64604801+Shivani-Madan@users.noreply.github.com>
2020-05-08 20:53:39 +0530

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.05.2020 03:13:07
-// Design Name: 
-// Module Name: tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 module tb;
    reg clk;
    reg [0:31] A;
